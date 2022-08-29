8f2fc02d7c8694f9c20a45d07009b5c9e411cfd9
Vitorjau <vitormjau@gmail.com>
2022-08-28 23:45:53 -0300

    commit

---
+++ codigo/Exercicio2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int cont = 0;
+            Console.WriteLine("Digite um numero: ");
+            numero = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= numero; i++){
+                if(numero %i == 0){
+                    cont++;
+                }
+            }
+            if(cont == 2){
+                Console.WriteLine("Numero é primo!");
+            }else{
+                Console.WriteLine("Numero não é primo!");
+            }
+
+        
+        }
+    }
+}
---
+++ codigo/Exercicio4/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Exercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+     int[,] mat = new int[5,3] { { 7, 8 ,9 }, { 5, 2 ,3}, { 7, 8 ,9 }, { 11, 12 ,13},{ 14, 15 ,16}};
+     int somal = 0, somac = 0;
+
+      System.Console.WriteLine(" Soma das linhas : ");
+         for(int l = 0; l < 5; l++){
+            for(int c = 0; c < 3; c++){
+               somal = somal + mat[l,c];
+            }
+               System.Console.WriteLine(somal);
+                 somal = 0;
+               System.Console.WriteLine();
+         }
+
+      System.Console.WriteLine("Soma das colunas: ");
+          for(int c = 0; c < 3; c++){
+             for(int l = 0; l < 5; l++){
+                somac = somac + mat[l,c];
+             }
+                System.Console.WriteLine(somac);
+                  somac = 0;
+                System.Console.WriteLine();
+          }
+        }
+    }
+}    
